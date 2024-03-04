6cad2fedd380372b6b75637caee5155e1a103e2d
Sunny <sunny.kumar@zomato.com>
2024-03-04 12:05:58 +0530

    App rename

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -3,7 +3,7 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication, title = "Simple todo") {
+    Window(onCloseRequest = ::exitApplication, title = "Foodverse") {
         App()
     }
 }
